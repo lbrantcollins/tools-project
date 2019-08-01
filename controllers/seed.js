2b2f5a889f6a9ecccc12f94414cbaeab7dc7123b
lbrantcollins <lbrantcollins@gmail.com>
2019-08-01 15:41:40 -0500

    allow database seeding

--- controllers/seed.js
+++ controllers/seed.js
@@ -7,24 +7,24 @@ const toolData 	= require("../public/js/seed")
 // Seeding Tools Collection and a few items
 // *****************************************************
 
-// router.get('/seed', async (req, res, next) => {
-// 	try {
-// 		const data = await Tool.collection.insertMany(toolData)
-// 		console.log("added provided tool data");
-// 		console.log(data);
-// 		const foundTools = await Tool.find({});
-// 		console.log(foundTools);
-// 		for(let i = 0; i < foundTools.length; i++){
-// 			Item.create({tool: foundTools[i]._id, rented: false});
-// 			Item.create({tool: foundTools[i]._id, rented: true});
-// 			Item.create({tool: foundTools[i]._id, rented: false});
-// 		}
-// 		res.redirect('/');
+router.get('/seed', async (req, res, next) => {
+	try {
+		const data = await Tool.collection.insertMany(toolData)
+		console.log("added provided tool data");
+		console.log(data);
+		const foundTools = await Tool.find({});
+		console.log(foundTools);
+		for(let i = 0; i < foundTools.length; i++){
+			Item.create({tool: foundTools[i]._id, rented: false});
+			Item.create({tool: foundTools[i]._id, rented: true});
+			Item.create({tool: foundTools[i]._id, rented: false});
+		}
+		res.redirect('/');
 		
-// 	} catch(err){
-// 		next(err)
-// 	}
-// });
+	} catch(err){
+		next(err)
+	}
+});
 
 
 
